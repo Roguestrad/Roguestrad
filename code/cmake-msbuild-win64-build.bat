c2852c6c1fc0012561c7a0ae1b4bd13595045aa5
Robert Beckebans <robert.beckebans@gmail.com>
2026-01-23 18:04:12 +0100

    Initial support for ZED editor + VS 2022 toolchain

---
+++ code/cmake-msbuild-win64-build.bat
@@ -0,0 +1,5 @@
+del /s /q build
+mkdir build
+cmake -B build -G "Visual Studio 17" -A x64 -DFFMPEG=OFF -DBINKDEC=ON .
+cmake -B build --build . --config Release
+pause
--- code/cmake-msbuild-win64.bat
+++ code/cmake-msbuild-win64.bat
@@ -1,7 +1,3 @@
-cd ..
 del /s /q build
 mkdir build
-cd build
-cmake -G "Visual Studio 17" -A x64 -DFFMPEG=OFF -DBINKDEC=ON ../code
-cmake --build . --config Release
-pause
+cmake -B build -G "Visual Studio 17" -A x64 -DFFMPEG=OFF -DBINKDEC=ON -DSTANDALONE=OFF .
